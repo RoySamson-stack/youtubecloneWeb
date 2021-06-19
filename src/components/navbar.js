c73d22d48fa1bdcf49710b98ee80d83d3ef46441
black-67 <reretsams@gmail.com>
2021-06-19 18:27:13 +0300

    sidenav update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,11 +4,7 @@ export default function NavBar() {
    return (
      
       <nav>
-         <NavLink
-         to=""
-         className=""
-         >Home
-         </NavLink>
+      Home
          </nav>
 
    )
--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -3,18 +3,85 @@ import { NavLink } from 'react-router-dom';
 
 export default function SideNav() {
    return (
-      <aside className="left-0">
-         <div className="">
-      <nav className="absolute z-10 w-48 bg-white-500 text-gray-700 min-h-screen border-b border-gray-700">
-               <ul class="mt-8">
-                  <li>
-                     <a href="" className="px-4 py-2 hover:bg-gray-200">
-                     Home
-                     </a>
-                  </li>
-               </ul>
-            </nav>
+      <nav className="navbar navbar-light fixed-top w-64">
+         <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+         <span className="navbar-toggler-icon navbar-dark"></span>
+         </button>
+        
+         <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ml-auto pt-4">
+               <hr className=""/>
+               <li className="navbar-item ">
+                  <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 bold mr-6 text-black hover:bg-gray-400 text-decoration:none"
+                  >Home</NavLink>
+               </li>
+               <li className="navbar-item">
+                 <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 mr-6 text-black hover:bg-gray-400"
+                  >Explore</NavLink>
+               </li>
+               <li className="navbar-item">
+                 <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 mr-6 text-black hover:bg-gray-400"
+                  >Subscription</NavLink>
+               </li>
+               <hr/>
+               <li className="navbar-item">
+                 <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 mr-6 text-black hover:bg-gray-400"
+                  >Library</NavLink>
+               </li>
+               <li className="navbar-item">
+                 <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 mr-6 text-black hover:bg-gray-400"
+                  >History</NavLink>
+                  
+               </li>
+               <li className="navbar-item">
+                 <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 mr-6 text-black hover:bg-gray-400"
+                  >Your videos</NavLink>
+               
+               </li>
+               <li className="navbar-item">
+                 <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 mr-6 text-black hover:bg-gray-400"
+                  >Watch later</NavLink>
+               
+               </li>
+               <li className="navbar-item">
+                 <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 mr-6 text-black hover:bg-gray-400"
+                  >Liked Videos</NavLink>
+               
+               </li>
+               <li className="navbar-item">
+                 <NavLink
+                     exact
+                     to="/"
+                     className="items-center mb-6 pr-2 pl-12 mr-6 text-black hover:bg-gray-400"
+                  >Show more</NavLink>
+                  
+               </li>
+            </ul>
          </div>
-      </aside>
-      )
+      </nav>
+   )
 }
