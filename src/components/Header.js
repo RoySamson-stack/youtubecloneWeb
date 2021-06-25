db4a720a51c2ffd033569d908a4df2efaf21b842
black-67 <reretsams@gmail.com>
2021-06-25 13:50:04 +0300

    moving

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import MenuIcon from "@material-ui/icons/Menu"
+import SearchIcon from "@material-ui/icons/Search"
+import VideoCallIcon from "@material-ui/icons/VideoCallIcon"
+import AppsIcon from "@material-ui/icons/Apps"
+import NotificationIcon from "@material-ui/icons/Notifications"
 
 function Header() {
   return (
     <div className="header">
-      <h1>Header</h1>
+    <h1>Header</h1>
+      <MenuIcon />
+      <img src="https://www.transparentpng.com/thumb/youtube-logo/hd-youtube-logo-image-5.png"
+        className="mt-3 ml-4"
+      />
+      <input type="text" className="border-2 border-gray-900"/>
+      <SearchIcon />
+      <VideoCallIcon />
+      <AppsIcon />
+      <NotificationIcon />
     </div>
   )
 }
